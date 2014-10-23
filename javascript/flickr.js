cfa3d508394daad7a9a23e46751d92ab32afe006
SamplifyLife <samplifyinglife@gmail.com>
2014-10-23 12:57:56 -0700

    multiple bug fixes and typo repairs

--- javascript/flickr.js
+++ javascript/flickr.js
@@ -1,22 +1,24 @@
 
 $(document).ready(function () {
-	$('button').click(function () {
+	$("form").submit(function(e) { 
+		e.preventDefault();
 		var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-		var searchWord = document.getElementById("query").value
-		var Options = {
+		var searchWord = document.getElementById("query").value;
+		var flickrOptions = {
 			tags: searchWord,
-			format: "json",
+			format: "json"
 		};
 		function displayPhotos(data) {
+
 			var photoHTML = '<ul>';
 			$.each( data.items, function (i, photo) {
-				photoHTML += '<li class="awesomePhoto">'
-				photoHTML += '<a href="' + photo.link + '" class="image">'
-				photoHTML += '<img src="' + photo.media.m + '"></a></li>'
+				photoHTML += '<li class="awesomePhoto">';
+				photoHTML += '<a href="' + photo.link + '" class="image">';
+				photoHTML += '<img src="' + photo.media.m + '"></a></li>';
 			});
 			photoHTML += '</ul>';
 			$('#photos').html(photoHTML);
 		};
 		$.getJSON(flickrAPI, flickrOptions, displayPhotos);
-	});	// end click
-});	// end ready
+	});	// end submit
+});
